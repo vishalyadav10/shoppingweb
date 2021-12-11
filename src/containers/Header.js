2de1b656464f45f43cdfa94b83828e601f73971c
Vishal <yadavvishal825@gmail.com>
2021-12-11 10:50:18 +0530

    intial commit

---
+++ src/containers/Header.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <div className="container">
+      <nav className="navbar navbar-expand-lg navbar-light bg-secondary">
+  <div className="container-fluid text-white">
+    <a className="navbar-brand text-white" href="/">Shopping app</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+        <li className="nav-item">
+          <a className="nav-link active text-white" aria-current="page" href="/">Home</a>
+        </li>
+        <li className="nav-item">
+          <a className="nav-link text-white" href="/contact">Contact Us</a>
+        </li>
+      
+      </ul>
+      <form className="d-flex">
+        <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button className="btn btn-outline-success text-white" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
+    </div>
+  );
+};
+
+export default Header;
